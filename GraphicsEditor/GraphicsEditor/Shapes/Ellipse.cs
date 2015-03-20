d7799a76e2e0e8d03426abaf9d382b2bd26e46a0
oleguhcok <oleguchok@gmail.com>
2015-03-20 23:07:49 +0300

    Initial commit with functional of first lab

---
+++ GraphicsEditor/GraphicsEditor/Shapes/Ellipse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsEditor
+{
+    class Ellipse : Shape
+    {
+        protected int length;
+        protected int width;
+
+        public Ellipse(int x, int y, int length, int width, Color brush) : base(x,y)
+        {
+            this.length = length;
+            this.width = width;
+            this.brush = new SolidBrush(brush);
+        }
+
+        /*public Ellipse(int x, int y, int length, Color brush) : base(x, y)
+        {
+            this.length = length;
+            this.width = length;
+            this.brush = new SolidBrush(brush);
+        }*/
+
+        /*private bool itIsCircle()
+        {
+            if (this.length == this.width)
+                return true;
+            else
+                return false;
+        }*/
+
+        public override void Draw(Graphics graphObj)
+        {
+            graphObj.FillEllipse(brush, point.X, point.Y, this.length, this.width);
+        }
+    }
+}
---
+++ GraphicsEditor/GraphicsEditor/Shapes/Shape.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GraphicsEditor
+{
+    abstract class Shape
+    {
+        protected Point point;
+        protected Brush brush;
+        protected Pen pen;
+
+        public Shape(int x, int y)
+        {
+            this.point = new Point(x, y);
+        }
+
+        public abstract void Draw(Graphics graphObj);
+
+        /*private virtual bool CorrectInput()
+        {
+            if (point.X < 0 || point.Y < 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }*/
+    }
+}
